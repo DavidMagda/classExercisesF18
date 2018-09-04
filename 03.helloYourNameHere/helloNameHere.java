78db43f8e368525e415141aabd18670e83b5882a
Greg <brosekerg@dewv.edu>
2018-09-04 14:04:34 -0400

    02 and 03

---
+++ 03.helloYourNameHere/helloNameHere.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+class helloNameHere {
+  public static void main(String args[]) {
+    Scanner philly = new Scanner(System.in);
+    System.out.println(philly.nextLine());
+  }
+}
+
+class enterYourAge {
+  public static void sub(String args[]) {
+    Scanner willy = new Scanner(System.in);
+    System.out.println(willy.nextLine());
+  }
+}
---
+++ 03.helloYourNameHere/helloNameHere.java~
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class helloNameHere {
+  public static void main(String args[]) {
+    Scanner philly = new Scanner(System.in);
+    System.out.println(philly.nextLine());
+  }
+}
