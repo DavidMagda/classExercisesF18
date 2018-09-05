dc2d19754ee7e85463d1c0d0796577dea0432209
Jonathan Stanley <stanleyj@dewv.edu>
2018-09-05 23:57:49 +0000

    In Class HW #5

---
+++ 05.fundamentalDatatypes/GPA.rb
@@ -0,0 +1,12 @@
+print "Enter the number of quality points earned: "
+quality_points = gets.to_f
+
+print "Enter the number of credit hours earned: "
+credit_hours = gets.to_f
+
+gpa = quality_points/credit_hours
+print "The grade point average is "
+print gpa
+
+
+
