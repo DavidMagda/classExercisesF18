7fee1ca217e19e5d660c25561651b70d92307f1d
Emre Yildirim <yildirime@dewv.edu>
2018-09-07 01:46:36 +0000

    Finished 5 Assignment

---
+++ 05.fundamentalDatatypes/Ageindays.py
@@ -0,0 +1,5 @@
+Age= input("Please write your age in years:")
+A= int(Age) * 365
+B= int(Age) * int(A) * 24
+C= int(Age) * int(B) * 60
+print("You are", A, "days old!\n You are" , B, "hours old!\n You are", C, "seconds old!")
---
+++ 05.fundamentalDatatypes/gpa.py
@@ -0,0 +1,3 @@
+points= input("Enter the number of quality points earned:")
+credits= int(input('Enter the number of credit hours earned:'))
+print(("GPA:"), int(points)/int(credits))
