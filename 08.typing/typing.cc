1b6615f4a6e2f8761087507dde802a89dae32fa3
Rayan Alkhuwaylid <alkhuwaylidr@dewv.edu>
2018-09-25 18:15:39 +0000

    08 typing

---
+++ 08.typing/typing.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int cats = 2;
+   int jobs = 3;
+   int reservations = 4;
+
+   int dollars = cats * jobs;
+if (reservations == dollars) 
+{
+
+    jobs = cats / 5;
+  
+}
+
+   return 0;
+}
---
+++ 08.typing/typing2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1
+    cout << i;
+    int i = 1.1
+    cout << i;
+    int i = "hello"
+    cout << i;
+    return 0;
+    
+}
