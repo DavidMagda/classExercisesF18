9319a252ce306ba791f8582f7f769758dacec8cc
David Magda <magdad@dewv.edu>
2018-09-25 16:37:00 +0000

    HW 08. Typing

---
+++ 08.typing/typing.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <?php 
+            $cats = 2;
+            $jobs = 3;
+            $reservations = 4;
+            
+            $dollars = $cats * $jobs;
+            
+            if ($reservations == $dollars) {
+                $jobs = $cats / 5;
+            }
+            
+            echo $cats
+        ?>
+    </body>
+</html>
---
+++ 08.typing/typing_2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <?php
+            $dynamicVariable = 1;
+            
+            echo $dynamicVariable;
+            
+            $dynamicVariable = 1.1;
+            
+            echo $dynamicVariable;
+            
+            $dynamicVariable = "Hello";
+            
+            echo $dynamicVariable;
+        ?>
+    </body>
+</html>
