c7351b389cb2a0dbccecb6f608ddbdbb2ee8a0d5
Steve Mattingly <mattinglys@dewv.edu>
2018-09-25 15:52:04 +0000

    Dynamic vs. static typing example

---
+++ 08.typing/dynamicTyping.js
@@ -0,0 +1,11 @@
+/* JS is dynamically typed, so all three values can be handled with one variable.
+ */
+ 
+let v = 1;
+console.log(typeof v); // "number"
+
+v = 1.1;
+console.log(typeof v); // "number"
+
+v = "hello";
+console.log(typeof v); // "string"
